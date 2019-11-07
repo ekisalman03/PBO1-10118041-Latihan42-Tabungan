02f5d93a1686640a0d4b78128962431ae3c16fcb
ekisalman03 <55838966+ekisalman03@users.noreply.github.com>
2019-11-07 22:25:13 +0700

    Add files via upload

---
+++ PBO1-10118041-Latihan42-Tabungan/src/pbo1/pkg10118041/latihan42/tabungan/PBO110118041Latihan42Tabungan.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118041.latihan42.tabungan;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 
+ * NAMA  : Eki salman sya'bani
+ * KELAS : IF 1
+ * NIM   : 10118041
+ * Deskripsi Program : Program ini untuk menampilkan output saldo tabungan.
+ */
+public class PBO110118041Latihan42Tabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner scn = new Scanner (System.in);
+        
+        System.out.println("=======Program Penarikan Uang=======");
+        System.out.print("Masukkan Saldo Awal : ");
+        Tabungan tabungan = new Tabungan(scn.nextInt());
+        System.out.print("Jumlah uang yang diambil : ");        
+        System.out.print("Saldo anda Sekarang : " 
+                + tabungan.ambilUang(scn.nextInt())+ "\n");
+    }
+    
+}
